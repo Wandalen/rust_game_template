ef47219d515f8d79ee0529d7af62731a2d12704b
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-11-08 13:14:14 +0200

    format main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ use winit::{
   window::Window,
 };
 
-async fn run(event_loop: EventLoop<()>, window: Window) {
+async fn run(event_loop : EventLoop<()>, window : Window)
+{
   let size = window.inner_size();
   let instance = wgpu::Instance::new(wgpu::Backends::all());
   let surface = unsafe { instance.create_surface(&window) };
   let adapter = instance
     .request_adapter(&wgpu::RequestAdapterOptions {
-      power_preference: wgpu::PowerPreference::default(),
-      force_fallback_adapter: false,
+      power_preference : wgpu::PowerPreference::default(),
+      force_fallback_adapter : false,
       // Request an adapter which can render to our surface
-      compatible_surface: Some(&surface),
+      compatible_surface : Some(&surface),
     })
     .await
     .expect("Failed to find an appropriate adapter");
@@ -23,10 +24,10 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
   let (device, queue) = adapter
     .request_device(
       &wgpu::DeviceDescriptor {
-        label: None,
-        features: wgpu::Features::empty(),
+        label : None,
+        features : wgpu::Features::empty(),
         // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
-        limits: wgpu::Limits::downlevel_webgl2_defaults().using_resolution(adapter.limits()),
+        limits : wgpu::Limits::downlevel_webgl2_defaults().using_resolution(adapter.limits()),
       },
       None,
     )
@@ -35,42 +36,42 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
   // Load the shaders from disk
   let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-    label: None,
-    source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+    label : None,
+    source : wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
   });
 
   let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-    label: None,
-    bind_group_layouts: &[],
-    push_constant_ranges: &[],
+    label : None,
+    bind_group_layouts : &[],
+    push_constant_ranges : &[],
   });
 
   let swapchain_format = surface.get_preferred_format(&adapter).unwrap();
 
   let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-    label: None,
-    layout: Some(&pipeline_layout),
-    vertex: wgpu::VertexState {
-      module: &shader,
-      entry_point: "vs_main",
-      buffers: &[],
+    label : None,
+    layout : Some(&pipeline_layout),
+    vertex : wgpu::VertexState {
+      module : &shader,
+      entry_point : "vs_main",
+      buffers : &[],
     },
-    fragment: Some(wgpu::FragmentState {
-      module: &shader,
-      entry_point: "fs_main",
-      targets: &[swapchain_format.into()],
+    fragment : Some(wgpu::FragmentState {
+      module : &shader,
+      entry_point : "fs_main",
+      targets : &[swapchain_format.into()],
     }),
-    primitive: wgpu::PrimitiveState::default(),
-    depth_stencil: None,
-    multisample: wgpu::MultisampleState::default(),
+    primitive : wgpu::PrimitiveState::default(),
+    depth_stencil : None,
+    multisample : wgpu::MultisampleState::default(),
   });
 
   let mut config = wgpu::SurfaceConfiguration {
-    usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-    format: swapchain_format,
-    width: size.width,
-    height: size.height,
-    present_mode: wgpu::PresentMode::Mailbox,
+    usage : wgpu::TextureUsages::RENDER_ATTACHMENT,
+    format : swapchain_format,
+    width : size.width,
+    height : size.height,
+    present_mode : wgpu::PresentMode::Mailbox,
   };
 
   surface.configure(&device, &config);
@@ -82,37 +83,40 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     let _ = (&instance, &adapter, &shader, &pipeline_layout);
 
     *control_flow = ControlFlow::Wait;
-    match event {
+    match event
+    {
       Event::WindowEvent {
         event: WindowEvent::Resized(size),
         ..
-      } => {
+      } =>
+      {
         // Reconfigure the surface with the new size
         config.width = size.width;
         config.height = size.height;
         surface.configure(&device, &config);
       }
-      Event::RedrawRequested(_) => {
+      Event::RedrawRequested(_) =>
+      {
         let frame = surface
           .get_current_texture()
           .expect("Failed to acquire next swap chain texture");
         let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label : None });
         {
           let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: None,
-            color_attachments: &[wgpu::RenderPassColorAttachment {
-              view: &view,
-              resolve_target: None,
-              ops: wgpu::Operations {
-                load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-                store: true,
+            label : None,
+            color_attachments : &[wgpu::RenderPassColorAttachment {
+              view : &view,
+              resolve_target : None,
+              ops : wgpu::Operations {
+                load : wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                store : true,
               },
             }],
-            depth_stencil_attachment: None,
+            depth_stencil_attachment : None,
           });
           rpass.set_pipeline(&render_pipeline);
-          rpass.draw(0..3, 0..1);
+          rpass.draw(0 .. 3, 0 .. 1);
         }
 
         queue.submit(Some(encoder.finish()));
@@ -122,12 +126,14 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         event: WindowEvent::CloseRequested,
         ..
       } => *control_flow = ControlFlow::Exit,
-      _ => {}
+      _ =>
+      {}
     }
   });
 }
 
-fn main() {
+fn main()
+{
   let event_loop = EventLoop::new();
   let window = winit::window::Window::new(&event_loop).unwrap();
   #[cfg(not(target_arch = "wasm32"))]
