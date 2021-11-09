faffce8b8668a3b1bc32ab226e683a82f2aff1bc
vmigdal <volodymyr.migdal@gmail.com>
2021-11-09 09:40:39 +0200

    android

---
+++ src/android.rs
@@ -0,0 +1,17 @@
+mod common;
+
+use common::*;
+
+#[cfg_attr(
+  target_os = "android",
+  ndk_glue::main(backtrace = "on", logger(level = "debug", tag = "rust-app"))
+)]
+#[allow(dead_code)]
+fn android_entry() {
+  
+  let event_loop = EventLoop::new();
+  let window = winit::window::Window::new(&event_loop).unwrap();
+
+  pollster::block_on(run(event_loop, window));
+
+}
