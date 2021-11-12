d04fac0d5776739c7e4093f9611b6c8861a7cec1
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-11-12 09:51:01 +0200

    style

--- src/android.rs
+++ src/android.rs
@@ -2,14 +2,12 @@ mod common;
 
 use common::*;
 
-#[cfg_attr(
-  target_os = "android",
-  ndk_glue::main(backtrace = "on", logger(level = "debug", tag = "rust-app"))
-)]
-#[allow(dead_code)]
-fn android_entry() {
+#[cfg_attr( target_os = "android", ndk_glue::main( backtrace = "on", logger( level = "debug", tag = "rust-app" ) ) )]
+#[allow( dead_code )]
+fn android_entry() 
+{
   let event_loop = EventLoop::new();
-  let window = winit::window::Window::new(&event_loop).unwrap();
+  let window = winit::window::Window::new( &event_loop ).unwrap();
 
-  pollster::block_on(run(event_loop, window));
+  pollster::block_on(run( event_loop, window ) );
 }
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,27 @@ mod common;
 
 use common::*;
 
-pub fn main() {
+pub fn main() 
+{
   let event_loop = EventLoop::new();
-  let window = winit::window::Window::new(&event_loop).unwrap();
+  let window = winit::window::Window::new( &event_loop ).unwrap();
 
-  #[cfg(not(target_arch = "wasm32"))]
+  #[cfg( not( target_arch = "wasm32" ) )]
   {
     // Temporarily avoid srgb formats for the swapchain on the web
-    pollster::block_on(run(event_loop, window));
+    pollster::block_on(run( event_loop, window ) );
   }
-  #[cfg(target_arch = "wasm32")]
+  #[cfg( target_arch = "wasm32" )]
   {
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    console_log::init().expect("could not initialize logger");
+    std::panic::set_hook( Box::new( console_error_panic_hook::hook ) );
+    console_log::init().expect( "could not initialize logger" );
     use winit::platform::web::WindowExtWebSys;
     // On wasm, append the canvas to the document body
     web_sys::window()
-      .and_then(|win| win.document())
-      .and_then(|doc| doc.body())
-      .and_then(|body| body.append_child(&web_sys::Element::from(window.canvas())).ok())
-      .expect("couldn't append canvas to document body");
-    wasm_bindgen_futures::spawn_local(run(event_loop, window));
+    .and_then( |win| win.document() )
+    .and_then( |doc| doc.body() )
+    .and_then( |body| body.append_child( &web_sys::Element::from( window.canvas() ) ).ok() )
+    .expect( "couldn't append canvas to document body" );
+    wasm_bindgen_futures::spawn_local( run( event_loop, window ) );
   }
 }
