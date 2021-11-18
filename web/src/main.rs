fd0e5b5fc531b1ed736fe0935be775451aa42ae5
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-11-18 19:56:26 +0200

    moving web target to own directory

---
+++ web/src/main.rs
@@ -0,0 +1,26 @@
+#[cfg( target_arch = "wasm32" )]
+
+use webgpu::common::*;
+
+use webgpu::winit::
+{
+  event_loop::{ EventLoop },
+  window::Window,
+  platform::web::WindowExtWebSys
+};
+
+pub fn main()
+{
+  let event_loop = EventLoop::new();
+  let window = Window::new( &event_loop ).unwrap();
+
+  std::panic::set_hook( Box::new( console_error_panic_hook::hook ) );
+  console_log::init().expect( "could not initialize logger" );
+  // On wasm, append the canvas to the document body
+  web_sys::window()
+  .and_then( |win| win.document() )
+  .and_then( |doc| doc.body() )
+  .and_then( |body| body.append_child( &web_sys::Element::from( window.canvas() ) ).ok() )
+  .expect( "couldn't append canvas to document body" );
+  wasm_bindgen_futures::spawn_local( run( event_loop, window ) );
+}
