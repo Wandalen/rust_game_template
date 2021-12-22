48028ec2ee7bcef21a90edf17a6e4045fabb0319
dmvict <dm.vict.kr@gmail.com>
2021-12-22 07:33:11 +0200

    Format code with `rustfmt`

--- module/renderer/src/main.rs
+++ module/renderer/src/main.rs
@@ -1,10 +1,10 @@
-#![ warn( missing_docs ) ]
+#![warn(missing_docs)]
 
 mod common;
 
 use crate::common::Renderer;
 
-#[cfg( target_arch = "wasm32" )]
+#[cfg(target_arch = "wasm32")]
 use winit::platform::web::WindowExtWebSys;
 
 // qqq : implement release build
@@ -13,16 +13,13 @@ struct App;
 
 impl common::Renderer for App
 {
-  fn new() -> App
-  {
-    App{}
-  }
+  fn new() -> App { App {} }
 
-  #[cfg( target_arch = "wasm32" )]
-  fn run( &self ) -> ()
+  #[cfg(target_arch = "wasm32")]
+  fn run(&self) -> ()
   {
     let event_loop = winit::event_loop::EventLoop::new();
-    let window = winit::window::Window::new( &event_loop ).unwrap();
+    let window = winit::window::Window::new(&event_loop).unwrap();
 
     /*
       qqq : where is that output from? why?
@@ -35,37 +32,31 @@ impl common::Renderer for App
       Init function: https://docs.rs/console_log/0.2.0/console_log/fn.init_with_level.html
       Levels: https://docs.rs/log/0.4.8/log/enum.Level.html
     */
-    console_log::init_with_level( log::Level::Warn ).expect( "Could not initialize logger" );
+    console_log::init_with_level(log::Level::Warn).expect("Could not initialize logger");
 
-    #[ cfg( debug ) ]
-    std::panic::set_hook( Box::new( console_error_panic_hook::hook ) );
+    #[cfg(debug)]
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     // panic!( "abc!" );
 
     web_sys::window()
-    .and_then( | win | win.document() )
-    .and_then( | doc | doc.body() )
-    .and_then( | body | body.append_child( &web_sys::Element::from( window.canvas() ) ).ok() )
-    .expect( "Couldn't append canvas to document body" );
-    wasm_bindgen_futures::spawn_local( common::run( event_loop, window ) );
+      .and_then(|win| win.document())
+      .and_then(|doc| doc.body())
+      .and_then(|body| body.append_child(&web_sys::Element::from(window.canvas())).ok())
+      .expect("Couldn't append canvas to document body");
+    wasm_bindgen_futures::spawn_local(common::run(event_loop, window));
   }
 }
 
 pub fn main()
 {
-  println!( "xxx" );
+  println!("xxx");
   let app = App::new();
   app.run();
 }
 
-#[cfg_attr( target_os = "ios", mobile_entry_point::mobile_entry_point ) ]
-pub fn ios_main()
-{
-  main();
-}
+#[cfg_attr(target_os = "ios", mobile_entry_point::mobile_entry_point)]
+pub fn ios_main() { main(); }
 
-#[cfg_attr( target_os = "android", ndk_glue::main( backtrace = "on" ) ) ]
-pub fn android_main()
-{
-  main();
-}
+#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
+pub fn android_main() { main(); }
