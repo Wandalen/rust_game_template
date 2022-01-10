f737d1236fda2b1db45e3fce314c19bc84a88d39
volodymyrmigdal <volodymyr.migdal@gmail.com>
2022-01-10 12:47:37 +0200

    miniquad: adjust format

--- module/renderer/miniquad/src/main.rs
+++ module/renderer/miniquad/src/main.rs
@@ -24,11 +24,11 @@ impl Stage
   pub fn new( ctx : &mut Context ) -> Stage
   {
     #[rustfmt::skip]
-    let vertices: [ Vertex; 4 ] =
+    let vertices : [ Vertex; 4 ] =
     [
         Vertex { pos : Vec2 { x : -0.5, y : -0.5 }, uv : Vec2 { x : 0., y : 0. } },
-        Vertex { pos : Vec2 { x :  0.5, y : -0.5 }, uv : Vec2 { x : 1., y : 0. } },
-        Vertex { pos : Vec2 { x :  0.5, y : 0.5 }, uv : Vec2 { x : 1., y : 1. } },
+        Vertex { pos : Vec2 { x : 0.5, y : -0.5 }, uv : Vec2 { x : 1., y : 0. } },
+        Vertex { pos : Vec2 { x : 0.5, y : 0.5 }, uv : Vec2 { x : 1., y : 1. } },
         Vertex { pos : Vec2 { x : -0.5, y : 0.5 }, uv : Vec2 { x : 0., y : 1. } },
     ];
     let vertex_buffer = Buffer::immutable( ctx, BufferType::VertexBuffer, &vertices );
@@ -36,7 +36,7 @@ impl Stage
     let indices : [ u16; 6 ] = [ 0, 1, 2, 0, 2, 3 ];
     let index_buffer = Buffer::immutable( ctx, BufferType::IndexBuffer, &indices );
 
-    let pixels: [ u8; 4 * 4 * 4 ] =
+    let pixels : [ u8; 4 * 4 * 4 ] =
     [
       0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF,
       0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00,
@@ -109,29 +109,31 @@ mod shader
 {
   use miniquad::*;
 
-  pub const VERTEX: &str = r#"#version 100
-    attribute vec2 pos;
-    attribute vec2 uv;
+  pub const VERTEX : &str = r#"
+  #version 100
+  attribute vec2 pos;
+  attribute vec2 uv;
 
-    uniform vec2 offset;
+  uniform vec2 offset;
 
-    varying lowp vec2 texcoord;
+  varying lowp vec2 texcoord;
 
-    void main()
-    {
-      gl_Position = vec4( pos + offset, 0, 1 );
-      texcoord = uv;
-    }"#;
+  void main()
+  {
+    gl_Position = vec4( pos + offset, 0, 1 );
+    texcoord = uv;
+  }"#;
 
-  pub const FRAGMENT: &str = r#"#version 100
-    varying lowp vec2 texcoord;
+  pub const FRAGMENT : &str = r#"
+  #version 100
+  varying lowp vec2 texcoord;
 
-    uniform sampler2D tex;
+  uniform sampler2D tex;
 
-    void main()
-    {
-      gl_FragColor = texture2D( tex, texcoord );
-    }"#;
+  void main()
+  {
+    gl_FragColor = texture2D( tex, texcoord );
+  }"#;
 
   pub fn meta() -> ShaderMeta
   {
