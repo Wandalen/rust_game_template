077f931870297d300d4987be817b444a74fd369a
volodymyrmigdal <volodymyr.migdal@gmail.com>
2022-01-10 12:33:03 +0200

    miniquad: format main file

--- module/renderer/miniquad/src/main.rs
+++ module/renderer/miniquad/src/main.rs
@@ -1,100 +1,114 @@
 use miniquad::*;
 
 #[repr(C)]
-struct Vec2 {
-    x: f32,
-    y: f32,
+struct Vec2
+{
+  x : f32,
+  y : f32,
 }
 #[repr(C)]
-struct Vertex {
-    pos: Vec2,
-    uv: Vec2,
+struct Vertex
+{
+  pos : Vec2,
+  uv : Vec2,
 }
 
-struct Stage {
-    pipeline: Pipeline,
-    bindings: Bindings,
+struct Stage
+{
+  pipeline : Pipeline,
+  bindings : Bindings,
 }
 
 impl Stage {
-    pub fn new(ctx: &mut Context) -> Stage {
-        #[rustfmt::skip]
-        let vertices: [Vertex; 4] = [
-            Vertex { pos : Vec2 { x: -0.5, y: -0.5 }, uv: Vec2 { x: 0., y: 0. } },
-            Vertex { pos : Vec2 { x:  0.5, y: -0.5 }, uv: Vec2 { x: 1., y: 0. } },
-            Vertex { pos : Vec2 { x:  0.5, y:  0.5 }, uv: Vec2 { x: 1., y: 1. } },
-            Vertex { pos : Vec2 { x: -0.5, y:  0.5 }, uv: Vec2 { x: 0., y: 1. } },
-        ];
-        let vertex_buffer = Buffer::immutable(ctx, BufferType::VertexBuffer, &vertices);
+  pub fn new( ctx : &mut Context ) -> Stage
+  {
+    #[rustfmt::skip]
+    let vertices: [ Vertex; 4 ] =
+    [
+        Vertex { pos : Vec2 { x : -0.5, y : -0.5 }, uv : Vec2 { x : 0., y : 0. } },
+        Vertex { pos : Vec2 { x :  0.5, y : -0.5 }, uv : Vec2 { x : 1., y : 0. } },
+        Vertex { pos : Vec2 { x :  0.5, y : 0.5 }, uv : Vec2 { x : 1., y : 1. } },
+        Vertex { pos : Vec2 { x : -0.5, y : 0.5 }, uv : Vec2 { x : 0., y : 1. } },
+    ];
+    let vertex_buffer = Buffer::immutable( ctx, BufferType::VertexBuffer, &vertices );
 
-        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
-        let index_buffer = Buffer::immutable(ctx, BufferType::IndexBuffer, &indices);
+    let indices : [ u16; 6 ] = [ 0, 1, 2, 0, 2, 3 ];
+    let index_buffer = Buffer::immutable( ctx, BufferType::IndexBuffer, &indices );
 
-        let pixels: [u8; 4 * 4 * 4] = [
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-            0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF,
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF,
-            0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-            0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-        ];
-        let texture = Texture::from_rgba8(ctx, 4, 4, &pixels);
+    let pixels: [ u8; 4 * 4 * 4 ] =
+    [
+      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF,
+      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00,
+      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF,
+      0xFF, 0xFF, 0xFF, 0xFF,
+    ];
+    let texture = Texture::from_rgba8( ctx, 4, 4, &pixels );
 
-        let bindings = Bindings {
-            vertex_buffers: vec![vertex_buffer],
-            index_buffer: index_buffer,
-            images: vec![texture],
-        };
+    let bindings = Bindings
+    {
+      vertex_buffers : vec![ vertex_buffer ],
+      index_buffer : index_buffer,
+      images : vec![ texture ],
+    };
 
-        let shader = Shader::new(ctx, shader::VERTEX, shader::FRAGMENT, shader::meta()).unwrap();
+    let shader = Shader::new( ctx, shader::VERTEX, shader::FRAGMENT, shader::meta() ).unwrap();
 
-        let pipeline = Pipeline::new(
-            ctx,
-            &[BufferLayout::default()],
-            &[
-                VertexAttribute::new("pos", VertexFormat::Float2),
-                VertexAttribute::new("uv", VertexFormat::Float2),
-            ],
-            shader,
-        );
+    let pipeline = Pipeline::new
+    (
+      ctx,
+      &[ BufferLayout::default() ],
+      &[
+        VertexAttribute::new( "pos", VertexFormat::Float2 ),
+        VertexAttribute::new( "uv", VertexFormat::Float2 ),
+      ],
+      shader,
+    );
 
-        Stage { pipeline, bindings }
+    Stage { pipeline, bindings }
+  }
+}
+
+impl EventHandler for Stage
+{
+  fn update( &mut self, _ctx: &mut Context ) {}
+
+  fn draw( &mut self, ctx: &mut Context )
+  {
+    let t = date::now();
+
+    ctx.begin_default_pass( Default::default() );
+
+    ctx.apply_pipeline( &self.pipeline );
+    ctx.apply_bindings( &self.bindings );
+    for i in 0..10
+    {
+      let t = t + i as f64 * 0.3;
+
+      ctx.apply_uniforms( &shader::Uniforms
+      {
+        offset : ( t.sin() as f32 * 0.5, ( t * 3.0 ).cos() as f32 * 0.5 ),
+      });
+      ctx.draw( 0, 6, 1 );
     }
+    ctx.end_render_pass();
+
+    ctx.commit_frame();
+  }
 }
 
-impl EventHandler for Stage {
-    fn update(&mut self, _ctx: &mut Context) {}
-
-    fn draw(&mut self, ctx: &mut Context) {
-        let t = date::now();
-
-        ctx.begin_default_pass(Default::default());
-
-        ctx.apply_pipeline(&self.pipeline);
-        ctx.apply_bindings(&self.bindings);
-        for i in 0..10 {
-            let t = t + i as f64 * 0.3;
-
-            ctx.apply_uniforms(&shader::Uniforms {
-                offset: (t.sin() as f32 * 0.5, (t * 3.).cos() as f32 * 0.5),
-            });
-            ctx.draw(0, 6, 1);
-        }
-        ctx.end_render_pass();
-
-        ctx.commit_frame();
-    }
+fn main()
+{
+  miniquad::start(conf::Conf::default(), | mut ctx |
+  {
+      UserData::owning( Stage::new( &mut ctx ), ctx )
+  })
 }
 
-fn main() {
-    miniquad::start(conf::Conf::default(), |mut ctx| {
-        UserData::owning(Stage::new(&mut ctx), ctx)
-    });
-}
+mod shader
+{
+  use miniquad::*;
 
-mod shader {
-    use miniquad::*;
-
-    pub const VERTEX: &str = r#"#version 100
+  pub const VERTEX: &str = r#"#version 100
     attribute vec2 pos;
     attribute vec2 uv;
 
@@ -107,7 +121,7 @@ mod shader {
         texcoord = uv;
     }"#;
 
-    pub const FRAGMENT: &str = r#"#version 100
+  pub const FRAGMENT: &str = r#"#version 100
     varying lowp vec2 texcoord;
 
     uniform sampler2D tex;
@@ -116,17 +130,21 @@ mod shader {
         gl_FragColor = texture2D(tex, texcoord);
     }"#;
 
-    pub fn meta() -> ShaderMeta {
-        ShaderMeta {
-            images: vec!["tex".to_string()],
-            uniforms: UniformBlockLayout {
-                uniforms: vec![UniformDesc::new("offset", UniformType::Float2)],
-            },
-        }
+  pub fn meta() -> ShaderMeta
+  {
+    ShaderMeta
+    {
+      images : vec![ "tex".to_string() ],
+      uniforms : UniformBlockLayout
+      {
+        uniforms : vec![ UniformDesc::new( "offset", UniformType::Float2 ) ],
+      },
     }
+  }
 
-    #[repr(C)]
-    pub struct Uniforms {
-        pub offset: (f32, f32),
-    }
+  #[repr(C)]
+  pub struct Uniforms
+  {
+    pub offset : ( f32, f32 ),
+  }
 }
